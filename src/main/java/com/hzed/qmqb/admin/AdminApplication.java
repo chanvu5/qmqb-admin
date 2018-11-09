604ea8403eef66254b8b8d26daa115cd9e16e63f
wuchengwu <wuchengbwu@hzed.com>
2018-11-09 16:32:05 +0800

    init

---
+++ src/main/java/com/hzed/qmqb/admin/AdminApplication.java
@@ -0,0 +1,12 @@
+package com.hzed.qmqb.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
