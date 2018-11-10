3c239c9404857498437e6f88da031283324ae0cc
wuchengwu <wuchengbwu@hzed.com>
2018-11-10 14:35:17 +0800

    generator生成表实体

--- src/main/java/com/hzed/qmqb/admin/AdminApplication.java
+++ src/main/java/com/hzed/qmqb/admin/AdminApplication.java
@@ -1,12 +1,26 @@
 package com.hzed.qmqb.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @SpringBootApplication
 public class AdminApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AdminApplication.class, args);
+        log.info("==========恭喜大佬，贺喜大佬，项目启动成功！==========");
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new com.alibaba.druid.pool.DruidDataSource();
     }
 }
